c19fb2ef33801e84473c1882c1dc47b558513c6a
raflishaista <raflishaista19@gmail.com>
2023-11-22 08:57:40 +0700

    authentication

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml,show_json,show_xml_by_id, show_json_by_id, register, login_user,logout_user, edit_product, get_product_json, create_ajax
+from main.views import show_main, create_product, show_xml,show_json,show_xml_by_id, show_json_by_id, register, login_user,logout_user, edit_product, get_product_json, create_ajax, create_product_flutter
 
 
  
@@ -21,4 +21,5 @@ urlpatterns = [
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-ajax/', create_ajax, name='create_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
